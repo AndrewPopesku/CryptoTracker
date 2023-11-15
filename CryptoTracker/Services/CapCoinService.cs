0558b55e329a5e36e47b1ddf022bbbe1e0bf0509
Andrew <andepopeskua@gmail.com>
2023-11-15 11:34:12 +0200

    created CryptoCurrency model and CapCoinService

---
+++ CryptoTracker/Services/CapCoinService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoTracker.Services
+{
+    internal class CapCoinService
+    {
+        private HttpClient _httpClient;
+        private static string _baseUrl = "api.coincap.io/v2/";
+
+        public CapCoinService()
+        {
+            _httpClient = new HttpClient();
+        }
+    }
+}
